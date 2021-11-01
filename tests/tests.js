ad3c74b77c694a57b9e746ff06be2c4c6960c35f
Tainan Felipe <tainanfelipe214@gmail.com>
2021-10-31 23:28:39 -0300

    Add tests

---
+++ tests/tests.js
@@ -0,0 +1,10 @@
+// The string:
+let text = "Hello world!";
+
+// Look for "Hello"
+let pattern1 = /Hello/g;
+let result1 = patt.test(text);
+
+// Look for "W3Schools"
+let pattern2 = /W3Schools/g;
+let result2 = pattern2.test(text);
